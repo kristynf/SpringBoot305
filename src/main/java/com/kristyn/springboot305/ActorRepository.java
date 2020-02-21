e798b02603d942ec36da81519d656c14fc88df69
kristynf <thetruthandsoul@gmail.com>
2020-02-20 20:17:45 -0500

    initial commit

---
+++ src/main/java/com/kristyn/springboot305/ActorRepository.java
@@ -0,0 +1,6 @@
+package com.kristyn.springboot305;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor,Long> {
+}
---
+++ src/test/java/com/kristyn/springboot305/Springboot305ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kristyn.springboot305;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot305ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
